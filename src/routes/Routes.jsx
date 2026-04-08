bc9f3146e06bd37b42976738a6b7e99e7baca66d
tahaimage8 <tahaimage8@gmail.com>
2026-04-08 14:57:46 +0600

    Project Setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout";
+import Books from "../pages/books/Books";
+import HomePage from "../pages/homepage/HomePage";
+
+export const router = createBrowserRouter([{
+  path: '/',
+  element: <MainLayout/>,
+  children:[
+    {
+      index : true,
+      element: <HomePage/>
+    },
+    {
+      path: 'books',
+      element: <Books/>,
+    }
+
+  ] 
+},
+
+
+])
